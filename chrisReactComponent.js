56fd751f2598bc7cbe76e110161765f560fb3003
MyNameIs <liu@cooper.edu>
2020-09-27 19:54:58 -0400

    9.27.2020 updates
    resume section information update

--- chrisReactComponent.js
+++ chrisReactComponent.js
@@ -116,7 +116,6 @@ class Education extends React.Component {
   render(){
     let renderArr = [];
     for(let items in this.schools){
-      // console.log(items["name"]);
       renderArr.push(
         <li key={items}>
             <span>{this.schools[items]["name"]}</span>
@@ -125,7 +124,188 @@ class Education extends React.Component {
         </li>
       );
     }
-    console.log(renderArr);
+    return(
+      <ul>
+        {renderArr}
+      </ul>
+    );
+  }
+}
+class WorkExperience extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  work_experience = {
+    fullstackA:{
+      position:"Cybersecurity Teaching Assisstant",
+      company: "Fullstack Academy",
+      year:"Jul. 2020 – Present",
+      description:["Guide students in workshop exercises, including: PWK, SANS challenges, forensics, incidence case studies",
+                  "Present lunchtime lectures about GitHub fundamentals and pentesting workflow",
+                  "Provide academic mentoring to a team of 5 students via weekly study groups and review lessons"
+                  ]
+    },
+    NYCDOT:{
+      position:"Project Engineer",
+      company:"NYC Department of Transportation, Movable Bridge",
+      year:"Jun. 2019 – Feb. 2020",
+      description:[" Researched and applied fire code compliance assessments to existing structures and proposed retrofits",
+                  "Identified and mediated design conflicts by reviewing design submittals, visiting project sites, and interfacing directly with city agencies, project stakeholders, and consultants",
+                  "Monitored $1.7 million budget and ensured adherence to contract schedule"
+                  ]
+    },
+    ENPOWER:{
+      position:"Energy Analyst and Field Intern",
+      company:"EN-POWER GROUP",
+      year:"Jun. 2018 – May 2019",
+      description:["Worked alongside lead engineers to perform ASHRAE Level II Energy Audits and write comprehensive reports of over 25 multifamily and commercial buildings in the NYC metro area for Local Law 87 compliance",
+                   "Developed framework to automate public database queries for retrieving property information; tested on a list of over 300 buildings and reduced task by 6 hours",
+                   "Prototyped tool streamlining report filing process using U.S. Dept. of Energy’s Building Asset Score API"
+                  ]
+    },
+    GraceChurchHS:{
+      position:"Math and Science Consultant",
+      company:"Grace Church High School",
+      year:"Sept. 2016 – May 2019",
+      description:["Tutored students in math, physics, and chemistry; acted as substitute teacher for classes up to 25 students"]
+    }
+    
+  }
+  render(){
+    let renderArr = [];
+    for(let items in this.work_experience){
+      let descriptionArr =[];
+      for(let i = 0, l = this.work_experience[items]["description"].length; i < l; i++){
+        descriptionArr.push(
+        <li key={Math.random()}>{this.work_experience[items]["description"][i]}</li>
+        );
+      }
+      renderArr.push(
+        <li key={items + (Math.random()*10)}>
+          <span className="positionName">{this.work_experience[items]["position"]}</span>
+          <p className="work_duration">{this.work_experience[items]["year"]}</p>
+          <p className="companyName">{this.work_experience[items]["company"]}</p>
+          <ul>
+            {descriptionArr}
+          </ul>
+        </li>
+      )
+    }
+    return(
+      <ul>
+        {renderArr}
+      </ul>
+    );
+  }
+}
+class ResearchProject extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  researchProjects={
+    project1:{
+      name:"Self-Propagating Computer Worm (Shellshock Vulnerability)",
+      addInfo:"Written in Python 3",
+      year:"Summer 2020",
+      description:["Developed program architecture: host discovery; vulnerability scan/identification; payload injection/execution",
+                  "Modular code structure enables integration with various payloads: proof-of-concept utilized botnet creation"]
+    },
+    project2:{
+      name:"Lossless Transmission Protocol Over Unreliable Channel",
+      addInfo:"ECE303, Communication Networks",
+      year:"Spring 2018",
+      description:["Designed a transport-layer protocol using Python which implements packet-checking for random bit errors packet loss, and packet duplication; sender and receiver work in coordination to ensure lossless transmission",
+                  "Implemented TCP-fairness via throughput throttling– sender/receiver pairs evenly share bandwidth, on average"]
+    },
+    project3:{
+      name:"Thermoelectrically Powered IP Camera Monitoring System",
+      link:"http://pubs.geothermal-library.org/lib/grc/1033759.pdf",
+      addInfo:"Hveragerði, Iceland",
+      year:"Summer 2017",
+      description:["Accomplished near-constant uptime of camera-router system without electrical grid access",
+                  "Led team of 8; delegated responsibilities and scheduled meetings with advisor to track group progress",
+                  "Contributed technical writing and figures to a Geothermal Resources Council"]
+    }
+  }
+  render(){
+    let renderArr = [];
+    for(let items in this.researchProjects){
+      let descriptionArr =[];
+      for(let i = 0, l = this.researchProjects[items]["description"].length; i < l; i++){
+        descriptionArr.push(
+        <li key={Math.random()}>{this.researchProjects[items]["description"][i]}</li>
+        );
+      }
+
+      if(this.researchProjects[items].hasOwnProperty("link")){
+        renderArr.push(
+        <li key={items}>
+          <a href={this.researchProjects[items]["link"]}><strong>{this.researchProjects[items]["name"]}</strong></a>
+          <p>{this.researchProjects[items]["addInfo"]}</p>
+          <p>{this.researchProjects[items]["year"]}</p>
+          <ul>
+            {descriptionArr}
+          </ul>
+        </li>
+        );
+      }
+      else{
+        renderArr.push(
+          <li key={items}>
+          <strong>{this.researchProjects[items]["name"]}</strong>
+          <p>{this.researchProjects[items]["addInfo"]}</p>
+          <p>{this.researchProjects[items]["year"]}</p>
+          <ul>
+            {descriptionArr}
+          </ul>
+        </li>
+        );
+      }
+    }
+    return(
+      <ul>
+        {renderArr}
+      </ul>
+    );
+  }
+}
+class LeadershipAct extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  activity = {
+    activity1:{
+      name:"CMD+CTRL Cyber Range CTF",
+      position:"First Place Team",
+      year:"Jun. 2020",
+      description:"(web app vulnerability testing)"
+    },
+    activity2:{
+      name:"Fullstack Cyber Hackathon",
+      position:"Second Place Team; Best Technical Report",
+      year:"May 2020",
+      description:"(pentesting, OSINT, pcap analysis, scripting challenges)"
+    },
+    activity3:{
+      name:"Splunk",
+      position:"Boss of the SOC: APT & Ransomware Scenarios; SANS Holiday Hack Challenge",
+      year:"Jun. 2020",
+      description:"(reconstruct simulated attacks via Cyber Kill Chain)"
+    },
+    activity4:{
+      name:"Event Supervisor",
+      position:"NYC East Regional Science Olympiad | Circuit Lab",
+      year:"2019, 2020"
+    },
+    activity5:{
+      name:"Captain",
+      position:"Cooper Union Varsity Men’s Volleyball Team",
+      year:"2017 – 2019"
+    }
+  }
+  render(){
+    let renderArr =[];
+    //resume coding here
     return(
       <ul>
         {renderArr}
@@ -139,9 +319,14 @@ class ResumeSection extends React.Component {
     super(props);
   }
   render() {
-    return <div>
+    return (
+    <div>
       <ResumeComponent title="Education" sectionItem={<Education />}/>
-    </div>;
+      <ResumeComponent title="Work Experience" sectionItem={<WorkExperience />} />
+      <ResumeComponent title="Research and Projects" sectionItem = {<ResearchProject />}/>
+      <ResumeComponent title="Activities and Leaderships" sectionItem = {<LeadershipAct />}/>
+    </div>
+    );
   }
 }
 // main render
